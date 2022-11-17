987cf6f212fcfbc433f16f43db57021f0ab1e7c6
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-11-17 05:26:40 +0000

    refractor using ES6 shorthand and function in client.js

--- play.js
+++ play.js
@@ -1,21 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-
-  return conn;
-};
-
+// using ES6 shorthand with "connect" being the entire function in client.js
 console.log("Connecting ...");
+const {connect} = require("./client.js")
 connect();
