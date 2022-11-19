e9c76002e6e514a502d7cff8ebf8194b511ce7a1
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-11-19 00:55:38 +0000

    keys assigned and global connection variable set

--- input.js
+++ input.js
@@ -1,17 +1,37 @@
+// Stores the active TCP connection object.
+let connection;
+
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
-  stdin.resume();
   stdin.on("data", handleUserInput);
   return stdin;
 };
+//function to take input from keyboard and send it to stdin
 const handleUserInput = function(data) {
-  // \u0003 maps to ctrl+c input
+  // \u0003 maps to ctrl+c input, keyboard keycodes
   if (data === '\u0003') {
    process.exit();
   }
+  if (data === '\u0077') {
+      console.log("Moved up");
+      connection.write("Move: up");  
+  }
+  if (data === "a") {
+    connection.write("Move: left");
+    console.log("Moved left");
+  }
+  if (data === "s") { 
+    connection.write("Move: down");
+    console.log("Moved down");
+  }
+  if (data === "d") {
+    connection.write("Move: right");
+    console.log("Moved right");
+  };
 };
 
 
